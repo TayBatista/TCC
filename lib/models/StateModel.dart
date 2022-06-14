960adedbe8056f3c7ae8815d20350d44017508b0
TayBatista <tayna_96.s2@hotmail.com>
2022-06-14 20:24:40 -0300

    Criando tela de splash Screen

--- lib/models/StateModel.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: file_names
-
-class StateModel {
-  StateModel({required this.regionId, required this.id, required this.name});
-
-  final int regionId;
-  final int id;
-  final String name;
-}
